d76eeb2884d862a4ab94a60146dcb35fa4c94c28
ldishman <132384939+ldishman@users.noreply.github.com>
2026-04-08 07:17:24 -0400

    Add first version with chipIds, tags, orphan padding, and aurora serv… (#10)
    * Add first version with chipIds, tags, orphan padding, and aurora service blocks
    
    * Revert "Add first version with chipIds, tags, orphan padding, and aurora service blocks"
    
    This reverts commit d47dd6e7073e8dfe575f06156b1b6466c78aa62b.
    
    * Add fresh-start version for chipId and tag bits
    
    * Add new .db cabling map
    
    ---------
    
    Co-authored-by: Lacey Dishman <ldishman@lxplus712.cern.ch>

--- EventFilter/Phase2PixelRawToDigi/plugins/BitStreamToAuroraProducer.cc
+++ EventFilter/Phase2PixelRawToDigi/plugins/BitStreamToAuroraProducer.cc
@@ -30,6 +30,7 @@ public:
 private:
   const edm::EDGetTokenT<edm::DetSetVector<Phase2ITChipBitStream>> ITChipBitStreamToken_;
   const unsigned int blockSize_;
+  const unsigned int serviceSize_;
   unsigned int eventCount_;
 
   // Buffer: detId -> [chip][event] -> bit stream data
@@ -40,6 +41,7 @@ BitStreamToAuroraProducer::BitStreamToAuroraProducer(const edm::ParameterSet& iC
     : ITChipBitStreamToken_(consumes<edm::DetSetVector<Phase2ITChipBitStream>>(
           iConfig.getParameter<edm::InputTag>("Phase2ITChipBitStream"))),
       blockSize_(iConfig.getParameter<unsigned int>("blockSize")),
+      serviceSize_(iConfig.getParameter<unsigned int>("serviceSize")),
       eventCount_(0) {
   produces<edm::DetSetVector<Phase2ITAuroraBitStream>>();
   produces<bool>("isComplete");
@@ -49,6 +51,7 @@ void BitStreamToAuroraProducer::fillDescriptions(edm::ConfigurationDescriptions&
   edm::ParameterSetDescription desc;
   desc.add<edm::InputTag>("Phase2ITChipBitStream", edm::InputTag("Phase2ITQCoreProducer"));
   desc.add<unsigned int>("blockSize", 16); // default event block size set to 16 (configurable 1-64)
+  desc.add<unsigned int>("serviceSize", 50); // default number of blocks until service block set to 50
   descriptions.add("BitStreamToAuroraProducer", desc);
 }
 
@@ -89,9 +92,21 @@ void BitStreamToAuroraProducer::produce(edm::Event& iEvent, const edm::EventSetu
 
       for (unsigned int c = 0; c < chips.size(); c++) {
         Phase2ITAuroraBitStream aurora(c, blockSize_);
+
         for (auto& evtBits : chips[c]) {
-          aurora.addEventBitStream(evtBits);
+          //aurora.addEventBitStream(evtBits);
+          
+          bool isStart = (&evtBits == &chips[c].front());  // true if this is the first event in this chip block
+          BitPacker p(isStart);
+
+          for (bool bit : evtBits) p.write(bit);  // write all event bits w/ headers (chipId and tag)
+          p.align(); // pad last partially filled word
+
+          // record total bits written in Aurora (actual bit values not needed)  
+          int totalBits = static_cast<int>(p.words.size() * 64);
+          aurora.addEventBitStream(std::vector<bool>(totalBits, false));
         }
+
         detset.push_back(std::move(aurora));
       }
 
