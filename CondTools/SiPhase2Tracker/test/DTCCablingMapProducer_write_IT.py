77a290b2ac4baa961c15276faaaad44d36dca25c
Sihyun Jeon <shjeon@cern.ch>
2026-05-21 18:26:55 +0200

    update

--- CondTools/SiPhase2Tracker/test/DTCCablingMapProducer_write_IT.py
+++ CondTools/SiPhase2Tracker/test/DTCCablingMapProducer_write_IT.py
@@ -24,34 +24,34 @@ process.PoolDBOutputService = cms.Service("PoolDBOutputService",
     )),
 )
 
-# InnerTrackerModulesToDTCs.csv column layout (19 columns):
-#   0  Module_DetId/i              6  N_Chips_Per_Module/I    12 LpGBT_Id/C
-#   1  Module_SubType/I            7  N_Channels_Per_Module/I 13 LpGBT_CMSSW_IdPerDTC/U
-#   2  Module_Section/C            8  Is_LongBarrel/O         14 MFB/I
-#   3  Module_Layer/I              9  Power_Chain/I           15 DTC_Id/I
-#   4  Module_Ring/I              10  Power_Chain_Type/C      16 DTC_CMSSW_Id/U
-#   5  Module_phi_deg/D           11  N_ELinks_Per_Module/I   17 IsPlusZEnd/O
-#                                                             18 IsPlusXSide/O
+#   0  Sensor_DetId/i              7  N_Chips_Per_Module/I    13 LpGBT_Id/C
+#   1  Module_DetId/i              8  N_Channels_Per_Module/I 14 LpGBT_CMSSW_IdPerDTC/U
+#   2  Module_SubType/I            9  Is_LongBarrel/O         15 MFB/I
+#   3  Module_Section/C           10  Power_Chain/I           16 DTC_Id/I
+#   4  Module_Layer/I             11  Power_Chain_Type/C      17 DTC_CMSSW_Id/U
+#   5  Module_Ring/I              12  N_ELinks_Per_Module/I   18 IsPlusZEnd/O
+#   6  Module_phi_deg/D                                       19 IsPlusXSide/O
+# Sensor_DetId is the key for inner trackers instead of Module_DetId
 process.otdtccablingmap_producer = cms.EDAnalyzer("DTCCablingMapProducer",
     record = cms.string('TrackerDetToDTCELinkCablingMapRcd'),
     modulesToDTCCablingCSVFileNames = cms.vstring(
-        "CondTools/SiPhase2Tracker/test/InnerTrackerModulesToDTCs.csv",
+        "CondTools/SiPhase2Tracker/test/it_cabling.csv",
     ),
-    csvFormat_ncolumns   = cms.uint32(19),
-    csvFormat_idetid     = cms.uint32(0),    # Module_DetId
-    csvFormat_idtcid     = cms.uint32(16),   # DTC_CMSSW_Id
-    csvFormat_igbtlinkid = cms.uint32(13),   # LpGBT_CMSSW_IdPerDTC
+    csvFormat_ncolumns   = cms.uint32(20),
+    csvFormat_idetid     = cms.uint32(0),    # Sensor_DetId (per-sensor key)
+    csvFormat_idtcid     = cms.uint32(17),   # DTC_CMSSW_Id
+    csvFormat_igbtlinkid = cms.uint32(14),   # LpGBT_CMSSW_IdPerDTC
     csvFormat_ielinkid   = cms.uint32(0),    # not in CSV -> dummy-fill
     dummy_fill_mode      = cms.string("DUMMY_FILL_ELINK_ID"),  # gbt from CSV, elink auto
 
     # per-module info
     read_innertracker_module_info = cms.bool(True),
-    csvFormat_inchips  = cms.uint32(6),
-    csvFormat_inelinks = cms.uint32(11),
-    csvFormat_isection = cms.uint32(2),
-    csvFormat_ilayer   = cms.uint32(3),
-    csvFormat_iring    = cms.uint32(4),
-    csvFormat_isubtype = cms.uint32(1),
+    csvFormat_inchips  = cms.uint32(7),
+    csvFormat_inelinks = cms.uint32(12),
+    csvFormat_isection = cms.uint32(3),
+    csvFormat_ilayer   = cms.uint32(4),
+    csvFormat_iring    = cms.uint32(5),
+    csvFormat_isubtype = cms.uint32(2),
 
     verbosity = cms.int32(0),
 )
